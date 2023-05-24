6104808d81606735affa35ebb2f94d1637185425
PIYUSH GHOSH <impiyushghosh@gmail.com>
2023-05-25 04:20:49 +0530

    created the home page, header and the courses page

--- course-mern/src/App.js
+++ course-mern/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from "./components/Home/Home";
-
+import Header from "./components/Layout/Header/Header"
+import Courses from "./components/Courses/Courses"
 function App() {
   return (
    <Router>
+   <Header />
        <Routes>
          <Route path="/" element={<Home />} />
+         <Route path="/courses" element={<Courses />} />
        </Routes>
    </Router>
     
