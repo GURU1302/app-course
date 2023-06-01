659bf844700fee28f66931743c4fa3b3fddfe17e
PIYUSH GHOSH <impiyushghosh@gmail.com>
2023-06-01 21:02:46 +0530

    added the register api and created new user and stored in to our database

--- course-mern-server/routes/userRoutes.js
+++ course-mern-server/routes/userRoutes.js
@@ -1,6 +1,11 @@
 import express from "express";
+import { register } from "../controllers/userController.js";
 
 
 const router = express.Router();
+
+router.route("/register").post(register);
+
+//login
 
 export default router;
