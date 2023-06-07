31414c46a0e8dd6c54a41881b2d07d942b6b9906
PIYUSH GHOSH <impiyushghosh@gmail.com>
2023-06-08 03:05:45 +0530

    added the edited code

--- course-mern-server/controllers/userController.js
+++ course-mern-server/controllers/userController.js
@@ -73,6 +73,9 @@ sendTokens(res,user,`Welcome Back, ${user.name}`,200);
 export const logout = catchAsyncError(async(req,res,next)=>{
     res.status(200).cookie("token",null,{
         expires: new Date(Date.now()),
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
     }).json({
         success:true,
         message:"Logged Out Successfully",
